75d7557d0e4b6695dc4d164fbb3e6f6b613ea440
Ahnuf Karim Chowdhury <123249294+Ahnuf-Karim-Chowdhury@users.noreply.github.com>
2026-09-22 22:17:15 +0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,3 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  base: './',
-});
+git add vite.config.js
+git commit -m "Fix: update base path for Vercel deployment"
+git push
